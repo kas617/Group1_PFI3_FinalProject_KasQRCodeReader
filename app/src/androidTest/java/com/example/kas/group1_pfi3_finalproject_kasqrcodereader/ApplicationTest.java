d9c58f6b01f2f930ddc6cb2b82e37fa8114b2d4d
kas617 <kas617@hotmail.com>
2015-05-11 15:09:09 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/kas/group1_pfi3_finalproject_kasqrcodereader/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.kas.group1_pfi3_finalproject_kasqrcodereader;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
